599da9a2060cf335142d681099b03a819721ef5e
lumoumou123 <20109114@mail.wit.ie>
2024-11-18 23:18:13 +0000

    Sorting features by rateing

--- movies/src/components/templateMovieListPage/index.js
+++ movies/src/components/templateMovieListPage/index.js
@@ -3,11 +3,16 @@ import Header from "../headerMovieList";
 import FilterCard from "../filterMoviesCard";
 import MovieList from "../movieList";
 import Grid from "@mui/material/Grid2";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
 
 function MovieListPageTemplate({ movies, title, action }) {
   const [nameFilter, setNameFilter] = useState("");
   const [genreFilter, setGenreFilter] = useState("0");
   const [releaseYearFilter, setReleaseYearFilter] = useState("");
+  const [sortOption, setSortOption] = useState("");
   const genreId = Number(genreFilter);
 
   let displayedMovies = movies
@@ -21,10 +26,17 @@ function MovieListPageTemplate({ movies, title, action }) {
       return releaseYearFilter ? m.release_date.startsWith(releaseYearFilter) : true;
     });
 
+  if (sortOption === "rating") {
+    displayedMovies.sort((a, b) => b.vote_average - a.vote_average);
+  } else if (sortOption === "rating_desc") {
+    displayedMovies.sort((a, b) => a.vote_average - b.vote_average);
+  }
+
   const handleChange = (type, value) => {
     if (type === "name") setNameFilter(value);
     else if (type === "genre") setGenreFilter(value);
     else if (type === "releaseYear") setReleaseYearFilter(value);
+    else if (type === "sort") setSortOption(value);
   };
 
   return (
@@ -32,11 +44,11 @@ function MovieListPageTemplate({ movies, title, action }) {
       <Grid size={12}>
         <Header title={title} />
       </Grid>
-      <Grid container sx={{flex: "1 1 500px"}}>
-        <Grid 
-          key="find" 
-          size={{xs: 12, sm: 6, md: 4, lg: 3, xl: 2}} 
-          sx={{padding: "20px"}}
+      <Grid container sx={{ flex: "1 1 500px" }}>
+        <Grid
+          key="find"
+          size={{ xs: 12, sm: 6, md: 4, lg: 3, xl: 2 }}
+          sx={{ padding: "20px" }}
         >
           <FilterCard
             onUserInput={handleChange}
@@ -44,6 +56,20 @@ function MovieListPageTemplate({ movies, title, action }) {
             genreFilter={genreFilter}
             releaseYearFilter={releaseYearFilter}
           />
+          <FormControl sx={{ marginTop: 2, minWidth: 220 }}>
+            <InputLabel id="sort-label">Sort By</InputLabel>
+            <Select
+              labelId="sort-label"
+              id="sort-select"
+              value={sortOption}
+              label="Sort By"
+              onChange={(e) => handleChange("sort", e.target.value)}
+            >
+              <MenuItem value="">None</MenuItem>
+              <MenuItem value="rating">Rating (High to Low)</MenuItem>
+              <MenuItem value="rating_desc">Rating (Low to High)</MenuItem>
+            </Select>
+          </FormControl>
         </Grid>
         <MovieList action={action} movies={displayedMovies}></MovieList>
       </Grid>
